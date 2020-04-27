c3492e20e2f4a5759f56a5a390a07c8d9c909e87
Alex Zhang <zgyghao@cn.ibm.com>
2020-04-27 11:58:09 +0800

    Update app, header

--- src/app/core/app/app.component.ts
+++ src/app/core/app/app.component.ts
@@ -5,6 +5,60 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements Oninit{
+  constructor(private router: Router){}
+  isFlight: boolean;
+  isDelayCode: boolean;
+  isDepartment: boolean;
+  isMvtAddress: boolean;
+  isSendTelex: boolean;
+  isTransLog: boolean;
+  isOutgoingTelex: boolean;
+  isUser: boolean;
+  sendTelexControl: boolean;
+
+  ngOnInit() {
+    this.sendTelexControl = false;
+  }
+
+  onClickNavBarLink() {
+    setTimeout(() => {
+      this.highlightActiveLink(this.router.url);
+    }, 50);
+  }
+
+  highlightActiveLink(routePath: string) {
+    this.isFlight = true;
+    this.isDelayCode = false;
+    this.isDepartment = false;
+    this.isMvtAddress = false;
+    this.isOutgoingTelex = false;
+    this.isSendTelex = false;
+    this.isUser = false;
+    this.isTransLog = false;
+    if (routePath.startsWith("/home/flight-list")) {
+      this.isFlight = true;
+    } else if (routePath.startsWith("/home/delay-codes")) {
+      this.isDelayCode = true;
+      this.isFlight = false;
+    } else if (routePath.startsWith("/home/department")) {
+      this.isDepartment = true;
+      this.isFlight = false;
+    } else if (routePath.startsWith("/home/outgoing-telex")) {
+      this.isOutgoingTelex = true;
+      this.isFlight = false;
+    } else if (routePath.startsWith("/home/user")) {
+      this.isUser = true;
+      this.isFlight = false;
+    } else if (routePath.startsWith("/home/transaction-log")) {
+      this.isTransLog = true;
+      this.isFlight = false;
+    } else if (routePath.startsWith("/home/send-telex")) {
+      this.isSendTelex = true;
+      this.isFlight = false;
+    } else if (routePath.startsWith("/home/movement-address")) {
+      this.isMvtAddress = true;
+      this.isFlight = false;
+    }
+  }
 }
